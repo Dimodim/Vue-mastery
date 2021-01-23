38b3f9a89f29edb5a8f48cc17d18354258f0ce45
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-23 15:34:45 +0200

    data binding

---
+++ Intro-to-Vue-2/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: "#app",
+    data(){
+        return {
+            socks: 'Socks',
+            description: 'Crisp socks that you can\'t miss on',
+            image1: './assets/socks_blue.jpg',
+            image2: './assets/socks_green.jpg',
+        }
+    }
+
+});
