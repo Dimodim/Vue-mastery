f010506a930593787955e7fcd564d71c6a4cb887
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-23 16:05:53 +0200

    v-if and v-else

--- Intro-to-Vue-2/main.js
+++ Intro-to-Vue-2/main.js
@@ -6,7 +6,14 @@ var app = new Vue({
             description: 'Crisp socks that you can\'t miss on',
             image1: './assets/socks_blue.jpg',
             image2: './assets/socks_green.jpg',
-            link: '#'
+            link: '#',
+            inStock: true,
+            onSale:true,
+            variants:[
+                {inventory:10,
+                image1: './assets/socks_blue.jpg',
+                }
+            ]
         };
     }
 
