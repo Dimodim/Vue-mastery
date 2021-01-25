594a5da9657bf122418069aaa9929a1640479660
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-25 13:06:14 +0200

    list rendering

--- Intro-to-Vue-2/main.js
+++ Intro-to-Vue-2/main.js
@@ -9,11 +9,23 @@ var app = new Vue({
             link: '#',
             inStock: true,
             onSale:true,
-            variants:[
-                {inventory:10,
-                image1: './assets/socks_blue.jpg',
+            variants: [
+                {
+                    id: 2234, 
+                    color: 'darkblue',
+                    image: './assets/images/socks_blue.jpg',
+                    quantity: 0,
+                    onSale: false
+                },
+                {
+                    id: 2235, 
+                    color: 'green',
+                    image: './assets/images/socks_green.jpg',
+                    quantity: 14,
+                    onSale: true
                 }
-            ]
+            ],
+            details:['cotton 80%','polyfills 15%', 'elastin 5%']
         };
     }
 
