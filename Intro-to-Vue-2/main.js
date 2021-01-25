3908f2675a348c7e718dae8f89ea8b18c5f8bf32
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-25 13:38:20 +0200

    event handling

--- Intro-to-Vue-2/main.js
+++ Intro-to-Vue-2/main.js
@@ -4,29 +4,37 @@ var app = new Vue({
         return {
             socks: 'Socks',
             description: 'Crisp socks that you can\'t miss on',
-            image1: './assets/socks_blue.jpg',
-            image2: './assets/socks_green.jpg',
             link: '#',
             inStock: true,
             onSale:true,
+            selectedVariant: 0,
             variants: [
                 {
                     id: 2234, 
                     color: 'darkblue',
-                    image: './assets/images/socks_blue.jpg',
+                    image: './assets/socks_blue.jpg',
                     quantity: 0,
                     onSale: false
                 },
                 {
                     id: 2235, 
                     color: 'green',
-                    image: './assets/images/socks_green.jpg',
+                    image: './assets/socks_green.jpg',
                     quantity: 14,
                     onSale: true
                 }
             ],
-            details:['cotton 80%','polyfills 15%', 'elastin 5%']
+            details:['cotton 80%','polyfills 15%', 'elastin 5%'],
+            cart: 0
         };
-    }
+    },
+    methods: {
+        addToCart: function(){
+            this.cart+=1;
+        },
+        updateVariant: function (index){
+            this.selectedVariant = index;
+        }
+    },
 
 });
