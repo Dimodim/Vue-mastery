7698a517372941159eaac6c4648d68431e4f8423
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-25 15:37:45 +0200

    components

--- Intro-to-Vue-2/main.js
+++ Intro-to-Vue-2/main.js
@@ -1,48 +1,94 @@
+Vue.component(
+    'product',{
+        props:{
+            premium:{
+                type:Boolean,
+                required: true
+            }
+        },
+        template:`
+        <div class="product">
+
+            <div class="product-image">
+                <a :href="link">
+                    <img :src="variants[selectedVariant].image"/>
+                </a>
+            </div>
+
+            <div class="product-info">
+                <h1>User is Premium: {{ premium }}</h1>
+                <h2 v-if="onSale">On Sale</h2>
+                <h1>{{ title }}</h1>
+                <span>{{ description }}</span>
+                <h3 v-if="inStock">In Stock</h3>
+                <h3 v-else-if="!inStock">Out Of Stock</h3>
+                <ul>
+                    <li v-for="(detail,index) in details" :key="index" >{{ detail }}</li>
+                </ul>
+               
+                <div @mouseover="updateVariant(index)" 
+                v-for="(variant,index) in variants" :key="variant.id" 
+                class="color-box" 
+                :style="{ backgroundColor: variant.color }">
+                </div>
+
+                
+                <button v-on:click="addToCart()" :disabled="!inStock" :class="{disabledButton: !inStock }">Add to cart</button>
+            </div>
+
+        </div>`,
+        data(){
+            return {
+                socks: 'Socks',
+                description: 'Crisp socks that you can\'t miss on',
+                link: '#',
+                onSale:true,
+                selectedVariant: 0,
+                variants: [
+                    {
+                        id: 2234, 
+                        color: 'darkblue',
+                        image: './assets/socks_blue.jpg',
+                        quantity: 0,
+                        onSale: false
+                    },
+                    {
+                        id: 2235, 
+                        color: 'green',
+                        image: './assets/socks_green.jpg',
+                        quantity: 14,
+                        onSale: true
+                    }
+                ],
+                details:['cotton 80%','polyfills 15%', 'elastin 5%'],
+            };
+        },
+        computed:{
+            title(){
+                return 'View Mastery ' + this.socks;
+            },
+            inStock(){
+                return  this.variants[this.selectedVariant].quantity > 0;
+              }
+        },
+        methods: {
+            addToCart: function(){
+                this.cart+=1;
+            },
+            updateVariant: function (index){
+                this.selectedVariant = index;
+            },
+            
+        },
+    }
+)
+
 var app = new Vue({
     el: "#app",
     data(){
         return {
-            socks: 'Socks',
-            description: 'Crisp socks that you can\'t miss on',
-            link: '#',
-            onSale:true,
-            selectedVariant: 0,
-            variants: [
-                {
-                    id: 2234, 
-                    color: 'darkblue',
-                    image: './assets/socks_blue.jpg',
-                    quantity: 0,
-                    onSale: false
-                },
-                {
-                    id: 2235, 
-                    color: 'green',
-                    image: './assets/socks_green.jpg',
-                    quantity: 14,
-                    onSale: true
-                }
-            ],
-            details:['cotton 80%','polyfills 15%', 'elastin 5%'],
+            premium: true,
             cart: 0
-        };
-    },
-    computed:{
-        title(){
-            return 'View Mastery ' + this.socks;
-        },
-        inStock(){
-            return  this.variants[this.selectedVariant].quantity > 0;
-          }
-    },
-    methods: {
-        addToCart: function(){
-            this.cart+=1;
-        },
-        updateVariant: function (index){
-            this.selectedVariant = index;
-        },
-        
-    },
-
+        }
+    }
 });
