5266d8c2cc0b07e0f0a43d5b8d34c4311e58bb12
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-28 19:11:49 +0200

    nested routes

--- Touring-Vue-Router/src/router/index.js
+++ Touring-Vue-Router/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EventList from '../views/EventList.vue'
-import EventDetails from '../views/EventDetails.vue'
+import EventList from '../views/event/EventList.vue'
+import EventDetails from '../views/event/EventDetails.vue'
+import EventRegister from '../views/event/EventRegister.vue'
+import EventEdit from '../views/event/EventEdit.vue'
+import EventLayout from '../views/event/Layout.vue'
 import About from '../views/About.vue'
 
 const routes = [
@@ -12,9 +15,26 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'EventDetails',
+    name: 'EventLayout',
     props: true,
-    component: EventDetails
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit
+      }
+    ]
   },
   {
     path: '/about',
---
+++ Touring-Vue-Router/src/views/event/EventEdit.vue
@@ -0,0 +1,8 @@
+<template>
+  <p>Edit the event here</p>
+</template>
+<script>
+export default {
+  props: ['event']
+}
+</script>
