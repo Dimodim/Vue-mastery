3857e321f06fe80708b483d3ca5db322938b0c62
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-02-02 15:38:26 +0200

    modules and succes and error notifications

--- Vuex/src/store/modules/event.js
+++ Vuex/src/store/modules/event.js
@@ -20,10 +20,24 @@ export const mutations = {
   }
 }
 export const actions = {
-  createEvent({ commit }, event) {
-    return EventService.postEvent(event).then(() => {
-      commit('ADD_EVENT', event)
-    })
+  createEvent({ commit, dispatch }, event) {
+    return EventService.postEvent(event)
+      .then(() => {
+        commit('ADD_EVENT', event)
+        const notification = {
+          type: 'success',
+          message: 'Your event has been created!'
+        }
+        dispatch('notification/add', notification, { root: true })
+      })
+      .catch(error => {
+        const notification = {
+          type: 'error',
+          message: 'There was a problem creating your event: ' + error.message
+        }
+        dispatch('notification/add', notification, { root: true })
+        throw error
+      })
   },
   fetchEvents({ commit }, { perPage, page }) {
     EventService.getEvents(perPage, page)
@@ -32,7 +46,11 @@ export const actions = {
         commit('SET_EVENTS', response.data)
       })
       .catch(error => {
-        console.log('There was an error:', error.response)
+        const notification = {
+          type: 'error',
+          message: 'There was a problem fetching events: ' + error.message
+        }
+        dispatch('notification/add', notification, { root: true })
       })
   },
   fetchEvent({ commit, getters }, id) {
@@ -45,7 +63,13 @@ export const actions = {
           commit('SET_EVENT', response.data)
         })
         .catch(error => {
-          console.log('There was an error:', error.response)
+          const notification = {
+            type: 'error',
+            message: 'There was a problem fetching an event: ' + error.message
+          }
+          dispatch('notification/add', notification, {
+            root: true
+          })
         })
     }
   }
