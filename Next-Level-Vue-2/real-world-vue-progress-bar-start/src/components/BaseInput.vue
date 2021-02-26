5fcb19407434605e09de206a14b60d9e659386d7
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-02-26 18:41:09 +0200

    reusable input base input

---
+++ Next-Level-Vue-2/real-world-vue-progress-bar-start/src/components/BaseInput.vue
@@ -0,0 +1,25 @@
+<template>
+      <div>
+        <label v-if="label">{{ label }}</label>
+        <input
+          @input="updateValue"
+          :value="value"
+          v-bind="$attrs"
+        />
+      </div>
+    </template>
+    
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    value: [String, Number],
+    label: String
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
