ea0423e4f5a59609a29a67fdc96eaea7a8df8ade
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-03-01 15:00:52 +0200

    reusable base select

---
+++ Next-Level-Vue-2/real-world-vue-progress-bar-start/src/components/BaseSelect.vue
@@ -0,0 +1,35 @@
+<template>
+      <div>
+      <label v-if="label">{{ label }}</label>
+       <select 
+          :value="value" 
+          @change="updateValue" 
+          v-bind="$attrs">
+            <option v-for="option in options"
+              :selected="option === value"
+              :value="option" 
+              :key="option">
+              {{ option }}
+            </option>
+        </select>
+      </div>
+    </template>
+    
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    value: [String, Number],
+    label: String,
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
