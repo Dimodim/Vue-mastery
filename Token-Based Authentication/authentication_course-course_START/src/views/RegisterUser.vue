cf46ceee42443d69a552fbdc9f3b83e4259f3da9
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-03-02 14:08:27 +0200

    initial commit

---
+++ Token-Based Authentication/authentication_course-course_START/src/views/RegisterUser.vue
@@ -0,0 +1,40 @@
+<template>
+      <div>
+        <form>
+          <label for="name">
+            Name:
+          </label>
+          <input v-model="name" type="text" name="name" value>
+    
+          <label for="email">
+            Email:
+          </label>
+          <input v-model="email" type="email" name="email" value>
+    
+          <label for="password">
+            Password:
+          </label>
+          <input v-model="password" type="password" name value>
+    
+          <button type="submit" name="button">
+            Register
+          </button>
+        </form>
+      </div>
+    </template>
+
+<script>
+    export default {
+         data () {
+        return {
+          name: '',
+          email: '',
+          password: ''
+        }
+      },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
