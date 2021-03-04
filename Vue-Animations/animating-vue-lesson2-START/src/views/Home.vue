fcf8610ecbe0b63e0d5fbf0e0d32ee6d835273ad
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-03-04 14:15:01 +0200

    page transitions

--- Vue-Animations/animating-vue-lesson2-START/src/views/Home.vue
+++ Vue-Animations/animating-vue-lesson2-START/src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="home">
+    <h1>This is Home View page</h1>
+
     <button @click="toggleModal">Open</button>
+
     <transition name="fade">
       <div v-if="isOpen" class="modal">
-        <button @click="toggleModal">Close</button>
+        <p>
+          <button @click="toggleModal">Close</button>
+        </p>
       </div>
     </transition>
+
   </div>
 </template>
 
@@ -25,16 +31,5 @@ export default {
 </script>
 
 <style>
-.fade-enter {
-  opacity: 0;
-}
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease-out;
-}
-
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
